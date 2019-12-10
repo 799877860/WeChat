00137bbd158769dde9c47deeb5393038b9ab3cb0
wys <wys@Ubuntu1905.localdomain>
2019-12-10 16:35:54 +0800

    第二次修改错误，单双引号问题

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -80,7 +80,7 @@ class WechatController extends Controller
             // 获取用户的openID
             $openid = $xml_obj->FromUserName;
             // 获取用户信息
-            $url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->access_token.'&openid='.$openid.'&lang=zh_CN";
+            $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->access_token.'&openid='.$openid.'&lang=zh_CN';
             $user_info = file_get_contents($url);       //
             file_put_contents('wx_user.log',$user_info,FILE_APPEND);
         }
