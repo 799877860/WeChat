3ab5b8c1f5db71d4448e72a295e75584ae81f55a
799877860 <799877860@qq.com>
2019-12-19 15:06:05 +0800

    有序集合投票

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -19,18 +19,18 @@ class VoteController extends Controller
 
         // 处理业务逻辑
         $openid = $user_info['openid'];
-        $key = 's:vote:wanyang';
+        $key = 'ss:vote:wanyang';
 
         //   TODO    判断是否已经投过    使用redis集合 或 有序集合
-        if (Redis::sIsMember($key,$user_info['openid'])){
+        if (Redis::zrank($key,$user_info['openid'])){
             echo "已经投过票了";
         }else{
-            Redis::Sadd($key,$openid);
+            Redis::Zadd($key,time(),$openid);
         }
-        $mumbers = Redis::Smembers($key);       // 获取所有投票人的openID
+        $mumbers = Redis::zRange($key,0,-1,true);       // 获取所有投票人的openID
+        echo "<pre>";print_r($mumbers);echo "</pre>";die;
         $total = Redis::Scard($key);        // 统计投票总人数
         echo "投票总人数：".$total;
-        echo "<pre>";print_r($mumbers);echo "</pre>";
 
     }
 
