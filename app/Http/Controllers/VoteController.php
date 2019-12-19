69cef189cb7805bc11b8a22fde9ea893aadd41a8
799877860 <799877860@qq.com>
2019-12-19 20:29:21 +0800

    保存用户信息

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -16,6 +16,11 @@ class VoteController extends Controller
         $data = $this->getAccessToken($code);
         // 获取用户信息
         $user_info = $this->getUserInfo($data['access_token'],$data['openid']);
+        // 保存用户信息
+        $userinfo_key = 'h:u:'.$data['openid'];
+        Redis::hMset($userinfo_key,$user_info);
+
+
 
         // 处理业务逻辑
         $openid = $user_info['openid'];
@@ -30,9 +35,14 @@ class VoteController extends Controller
         $total = Redis::zCard($key);        // 获取总数
         echo '投票总人数： '.$total;echo "</br>";
         $mumbers = Redis::zRange($key,0,-1,true);       // 获取所有投票人的openID
-        echo "<pre>";print_r($mumbers);echo "</pre>";
+        echo "<pre>";print_r($mumbers);echo "</pre>";echo "<hr>";
         foreach ($mumbers as $k=>$v){
             echo "用户： ".$k . ' 投票时间：'. date('Y-m-d H:i:s',$v);echo "</br>";
+            $u_k = 'h:u:'.$k;
+            // $u = Redis::hgetAll($u_k);
+            $u = Redis::hMget($u_k,['openid','nickname','sex']);
+            echo "<pre>";print_r($u);echo "</pre>";
+
         }
     }
 
@@ -68,4 +78,24 @@ class VoteController extends Controller
         return $data;       // 返回用户信息
 
     }
+
+    public function hashTest()
+    {
+        $uid = 1000;
+        $key = 'h:user_info:'.$uid;
+        $user_info = [
+            'uid'         => $uid,
+            'user_name' => 'wys',
+            'email'      => 'wys@qq.com',
+            'age'         => 21,
+            'sex'         => 1,
+        ];
+
+        Redis::hMset($key,$user_info);die;
+        echo "</br>";
+        $u = Redis::hGetAll($key);
+        echo "<pre>";print_r($u);echo "</pre>";
+
+    }
+
 }
