709038a442d826da24a0d48440109a85d13406b0
799877860 <799877860@qq.com>
2019-12-20 23:19:58 +0800

    商品管理

--- app/Http/Controllers/Goods/IndexController.php
+++ app/Http/Controllers/Goods/IndexController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Goods;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Model\GoodsModel;
 class IndexController extends Controller
 {
-    public function detail()
+    public function detail(Request $request)
     {
-        return view('goods.detail');
+        $goods_id = $request->input('id');
+        $goods = GoodsModel::find($goods_id);
+        // echo "<pre>";print_r($goods->toArray());echo "</pre>";
+        $data = [
+            'goods' => $goods
+        ];
+        return view('goods.detail',$data);
     }
 }
